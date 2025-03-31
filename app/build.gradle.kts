ca107d9af71ba091be98073cf96f6be01c46adc9
FXgo <134941915+vladimir212455@users.noreply.github.com>
2025-03-31 10:33:50 +0700

    gradle ver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.ships_version2"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.ships_version2"
-        minSdk = 34
-        targetSdk = 34
+        minSdk = 35
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
