1922b19e806741ea0d01f78fdb30917d7c716a07
FXgo <134941915+vladimir212455@users.noreply.github.com>
2025-04-20 09:46:52 +0700

    6

--- app/src/main/java/com/example/ships_version2/AppCompactAtcivity/MainActivity.java
+++ app/src/main/java/com/example/ships_version2/AppCompactAtcivity/MainActivity.java
@@ -3,30 +3,52 @@ package com.example.ships_version2.AppCompactAtcivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import com.example.ships_version2.Adapters.AuthManager;
+import com.example.ships_version2.R;
 import com.example.ships_version2.Service.Media_service;
 import com.example.ships_version2.databinding.ActivityMainBinding;
+import com.example.ships_version2.fragments.Fragment_settings;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     static Intent intent;
-
+    public static Boolean sound;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
         AuthManager authManager = new AuthManager(this);
+        sound = true;
+        binding.settings.setOnClickListener(view ->
+        {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            Fragment fragment = fragmentManager.findFragmentById(R.id.settings_container);
+
+            if(fragment==null) {
+                fragment = new Fragment_settings();
+                fragmentManager.beginTransaction()
+                        .addToBackStack(String.valueOf(this))
+                        .replace(R.id.settings_container, fragment)
+                        .commit();
+            }
+        });
+
         binding.play.setOnClickListener(view-> {
+            if (sound) {
+                startService(new Intent(this, Media_service.class));
+            }
             authManager.logoutUser();
             Intent intent = new Intent(MainActivity.this, AuthorizationActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
             finish();
         });
-
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/ships_version2/fragments/Fragment_settings.java
@@ -0,0 +1,102 @@
+package com.example.ships_version2.fragments;
+
+import static com.example.ships_version2.AppCompactAtcivity.MainActivity.sound;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.ships_version2.databinding.FragmentFieldBinding;
+import com.example.ships_version2.databinding.FragmentSettingsBinding;
+
+public class Fragment_settings extends Fragment {
+    private final static String LOG_TAG = "ContentFragment_settings";
+    FragmentSettingsBinding binding;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.soundCheck.setChecked(true);
+        binding.soundCheck.setOnClickListener(view1 ->
+        {
+            if(sound)
+            {
+                sound = false;
+            }
+            else{sound = true;}
+        });
+    }
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        super.onViewStateRestored(savedInstanceState);
+        Log.d(LOG_TAG, "onViewStateRestored");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(LOG_TAG, "onDestroyView");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        Log.d(LOG_TAG, "onDetach");
+    }
+}
